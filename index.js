c2fd2df2dcc1a3a6c1c8b28cd0b2c9a3da0e3296
jaguar-paw33 <mr.priyankmishra@gmail.com>
2021-07-25 01:12:22 +0530

    setup push to heroku

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(express.static('./assets'));
 
@@ -9,12 +9,12 @@ app.set('views', './views');
 
 app.use("/", require('./routes'));
 
-app.listen(process.env.PORT || port, function(err){
+app.listen(port, function(err){
     if(err){
         console.log(`Error in running the Server : ${err}`);
         return;
     }
-    console.log(`Server is up and running on Port : ${this.address().port}`);
+    console.log(`Server is up and running on Port : ${port}`);
 })
 
 

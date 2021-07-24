0fc61bbba9bbba4ad5d19a6ae534d635a6135bea
jaguar-paw33 <mr.priyankmishra@gmail.com>
2021-07-25 00:03:06 +0530

    Deploy using NodeJS

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.use(express.static('./assets'));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use("/", require('./routes'));
+
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error in running the Server : ${err}`);
+        return;
+    }
+    console.log(`Server is up and running on Port : ${port}`);
+})
+
+
